a6dea0d1f64b65f9cd19720a98fb96b1d6e4f746
jipdori <94859523+jipdori@users.noreply.github.com>
2022-03-17 19:56:20 +0100

    Add files via upload

---
+++ Projecte-2 (codi modificat)/gimnasio/src/gimnasio/Nie.java
@@ -0,0 +1,77 @@
+
+package gimnasio;
+
+class Nie {
+    
+    private String nie;
+    private int numero;
+    private char lletra;
+
+    /*public Nie(String nie, int numero, char lletra) {
+        this.nie = nie;
+        this.numero = numero;
+        this.lletra = lletra;
+    }*/
+    
+    Nie(String nie) {
+        this.nie = nie;
+    }
+
+    Nie() {
+        
+    }
+
+    public boolean validarNie(String nie){
+        
+        boolean esValid = false;
+        int i=0;
+        int caracterASCII = 0;
+        char lletra=' ';
+        int mynie = 0;
+        int resto;
+        char[] assignacioLletra = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+        
+        if (nie.length() == 9 && Character.isLetter(nie.charAt(8))){
+            
+            do{
+                caracterASCII = nie.codePointAt(i);
+                esValid = (caracterASCII > 47 && caracterASCII < 58);
+                i++;
+            }
+            while(i < nie.length() - 1 && esValid);
+            
+        }
+        
+        if(esValid){
+            lletra = Character.toUpperCase(nie.charAt(8));
+            mynie = Integer.parseInt(nie.substring(0,8));
+            resto= mynie % 23;
+            esValid = (lletra == assignacioLletra[resto]);
+        }
+
+        return esValid;
+    
+    }
+
+    public String getNie() {
+        return nie;
+    }
+
+    public void setNie(String nie) {
+        this.nie = nie;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setLletra(char lletra) {
+        this.lletra = lletra;
+    }
+
+    @Override
+    public String toString() {
+        return nie + numero + lletra;
+    }
+
+}
